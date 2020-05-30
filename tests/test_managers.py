2b22498e7a90a2f9cda97d3d23f6bf01429823de
Omar Cardia <oc@bendingspoons.com>
2020-05-30 09:55:37 +0200

    test(managers): cover partially hitten branch

--- tests/test_managers.py
+++ tests/test_managers.py
@@ -24,3 +24,8 @@ def test_app_configuration_mongo_failure() -> None:
         mock.return_value = None
         with raises(ImmuniException):
             managers.app_configuration_mongo
+
+
+async def test_teardown_on_uninitialized() -> None:
+    uninitialized_managers = Managers()
+    await uninitialized_managers.teardown()
